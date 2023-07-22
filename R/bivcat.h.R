99c9e035000008ca3bba48b589fca94ad328807e
DLEIVA <dleivaur@gmail.com>
2023-07-22 15:07:43 +0200

    Update R files

--- R/bivcat.h.R
+++ R/bivcat.h.R
@@ -87,7 +87,8 @@ bivcatBase <- if (requireNamespace("jmvcore", quietly=TRUE)) R6::R6Class(
                 revision = revision,
                 pause = NULL,
                 completeWhenFilled = FALSE,
-                requiresMissings = FALSE)
+                requiresMissings = FALSE,
+                weightsSupport = 'auto')
         }))
 
 #' Categorical Data
--- R/ordsummary.h.R
+++ R/ordsummary.h.R
@@ -367,7 +367,8 @@ ordSummaryBase <- if (requireNamespace("jmvcore", quietly=TRUE)) R6::R6Class(
                 revision = revision,
                 pause = NULL,
                 completeWhenFilled = FALSE,
-                requiresMissings = FALSE)
+                requiresMissings = FALSE,
+                weightsSupport = 'auto')
         }))
 
 #' Ordinal Data
