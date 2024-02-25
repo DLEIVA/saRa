b9461976187591eab3c0fb1a0d6f702e2d477474
DLEIVA <dleivaur@gmail.com>
2024-02-25 10:13:57 +0100

    Adding R files for probability module

---
+++ R/discvars.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+discvarsClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "discvarsClass",
+    inherit = discvarsBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
